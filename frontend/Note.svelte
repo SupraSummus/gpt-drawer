0135b8eda5a1716b7ccc34f19f4269b07001cfce
Jan Rydzewski <flegmer@gmail.com>
2023-02-05 01:59:35 +0100

    Editing notes

--- frontend/Note.svelte
+++ frontend/Note.svelte
@@ -1,25 +1,83 @@
 <script>
   import axios from 'axios'
-	import { createEventDispatcher } from 'svelte'
+	import { createEventDispatcher, onMount } from 'svelte'
 
-  let id
+  let id = null
+  let title = ''
+  let content = ''
+
   let dispatch = createEventDispatcher()
+  let state = 'fetching'
 
-  $: note_data_promise = axios.get(`/api/notes/${id}/`).then(response => response.data)
+  function load_from_response(response) {
+    const data = response.data
+    console.assert(data.id === id)
+    title = data.title
+    content = data.content
+    state = 'view'
+  }
+
+  async function save_note() {
+    state = 'saving'
+    const response = await axios.patch(`/api/notes/${id}/`, {
+      title,
+      content,
+    })
+    load_from_response(response)
+  }
+
+  async function fetch_note() {
+    state = 'fetching'
+    const response = await axios.get(`/api/notes/${id}/`)
+    load_from_response(response)
+  }
+
+  onMount(() => {
+    fetch_note()
+  })
 
   export { id }
 </script>
 
-<article>
+<article on:dblclick={() => {
+  if (state === 'view') state = 'edit'
+}} aria-busy="{state === 'saving' || state === 'fetching'}">
   <header>
-    <a href aria-label="Close" class="close"
-      on:click|preventDefault={() => dispatch('close', id)}
-    >Close</a>
-    <h2 class="title">{#await note_data_promise then data} {data.title} {/await}</h2>
+    
+    <div class="actions">
+      {#if state === 'edit'}
+        <a href on:click|preventDefault={() => save_note()}>
+          Save
+        </a>
+        <a href on:click|preventDefault={() => fetch_note()}>
+          Cancel
+        </a>
+      {:else if state === 'view'}
+        <a href on:click|preventDefault={() => {state = 'edit'}}>
+          Edit
+        </a>
+      {/if}
+      
+      <a href class="close"
+        on:click|preventDefault={() => dispatch('close', id)}
+      >
+        Close
+      </a>
+    </div>
+
+    {#if state === 'edit'}
+      <h2 class="title" contenteditable bind:textContent={title}></h2>
+    {:else}
+      <h2 class="title">{title}</h2>
+    {/if}
   </header>
-  <p class="content">
-    {#await note_data_promise then data} {data.content} {/await}
-  </p>
+
+  {#if state === 'edit'}
+    <p class="content" contenteditable bind:textContent={content}></p>
+  {:else}
+    <p class="content">{content}</p>
+  {/if}
+
 </article>
 
 <style>
@@ -27,7 +85,7 @@
     margin-bottom: 0;
   }
 
-  .close {
+  .actions {
     float: right;
   }
 
@@ -37,5 +95,10 @@
 
   .content {
     margin: 0;
+  }
+
+  [contenteditable] {
+    outline: none;
+    border: 1px solid red;
   }
 </style>
